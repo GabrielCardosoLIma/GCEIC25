170d1d913ac32f0b27157f94da779d941138b58b
Isabella <117479295+isabellatressino@users.noreply.github.com>
2025-05-21 16:05:24 -0300

    Inicializando eslint

---
+++ api/routes/calculoRoute.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+
+function calcularAposentadoria(idade, contribuicao, sexo) {
+  const idadeMinima = sexo === 'F' ? 62 : 65;
+  const contribuicaoMinima = 15;
+
+  if (idade >= idadeMinima && contribuicao >= contribuicaoMinima) {
+    return {
+      podeAposentar: true,
+      mensagem: "Você já pode se aposentar."
+    };
+  } else {
+    const faltaIdade = Math.max(idadeMinima - idade, 0);
+    const faltaContribuicao = Math.max(contribuicaoMinima - contribuicao, 0);
+
+    let mensagens = [];
+
+    if (faltaIdade > 0) {
+      mensagens.push(`${faltaIdade} ano(s) de idade`);
+    }
+
+    if (faltaContribuicao > 0) {
+      mensagens.push(`${faltaContribuicao} ano(s) de contribuição`);
+    }
+
+    return {
+      podeAposentar: false,
+      mensagem: `Você ainda não pode se aposentar. Faltam ${mensagens.join(' e ')}.`
+    };
+  }
+}
+
+router.post('/calculo', (req, res) => {
+  const { idade, contribuicao, sexo } = req.body;
+
+  // Validação dos dados
+  if (
+    typeof idade !== 'number' ||
+    typeof contribuicao !== 'number' ||
+    typeof sexo !== 'string' ||
+    !['M', 'F'].includes(sexo) ||
+    idade <= 0 ||
+    contribuicao < 0
+  ) {
+    return res.status(400).json({
+      erro: true,
+      mensagem: 'Por favor, envie idade, contribuição válidos e sexo ("M" ou "F").'
+    });
+  }
+
+  const resultado = calcularAposentadoria(idade, contribuicao, sexo);
+  return res.status(200).json(resultado);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -10,15 +10,18 @@ const port = 3000;
 // importa as rotas
 const userRoutes = require('./routes/userRoutes')
 const baseRoutes = require('./routes/baseRoutes')
+const calculoRoute = require('./routes/calculoRoute')
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', baseRoutes);
 app.use('/users', userRoutes);
+app.use('/api', calculoRoute);
 
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
   console.log(`Swagger em http://localhost:${port}/api-docs`);
 });
+
