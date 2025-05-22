ef2f4849cb6c809bb401285d4059f53046c1b048
Gc20040702 <limag656@gmail.com>
2025-05-22 09:25:12 -0300

    refactor: refactor function calcularRegra and route regras

--- api/routes/aposRoutes.js
+++ api/routes/aposRoutes.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { calcularAposentadoria } = require('../controllers/aposController');
+const { calcularAposentadoria } = require("../controllers/aposController");
+const { calcularRegra } = require("../controllers/aposController");
 
-router.post('/calculo', (req, res) => {
+router.post("/calculo", (req, res) => {
   const { idade, contribuicao, sexo } = req.body;
 
   if (
-    typeof idade !== 'number' ||
-    typeof contribuicao !== 'number' ||
-    typeof sexo !== 'string' ||
-    !['M', 'F'].includes(sexo) ||
+    typeof idade !== "number" ||
+    typeof contribuicao !== "number" ||
+    typeof sexo !== "string" ||
+    !["M", "F"].includes(sexo) ||
     idade <= 0 ||
     contribuicao < 0
   ) {
     return res.status(400).json({
       erro: true,
-      mensagem: 'Por favor, envie idade, contribuição válidos e sexo ("M" ou "F").'
+      mensagem:
+        'Por favor, envie idade, contribuição válidos e sexo ("M" ou "F").',
     });
   }
 
@@ -24,9 +26,35 @@ router.post('/calculo', (req, res) => {
   return res.status(200).json(resultado);
 });
 
-module.exports = router;
+router.post("/regras", (req, res) => {
+  try {
+    const { sexo, idade, contribuicao, categoria } = req.body;
 
-const { calcularRegra } = require('../controllers/aposController');
-router.post('/regras', calcularRegra);
+    if (
+      typeof idade !== "number" ||
+      typeof contribuicao !== "number" ||
+      typeof sexo !== "string" ||
+      typeof categoria !== "string" ||
+      !["M", "F"].includes(sexo) ||
+      idade <= 0 ||
+      contribuicao < 0
+    ) {
+      return res.status(400).json({
+        erro: true,
+        mensagem:
+          'Por favor, envie idade, contribuição, categoria válidos e sexo ("M" ou "F").',
+      });
+    }
+
+    const regras = calcularRegra(sexo, idade, contribuicao, categoria);
+
+    return res.status(200).json(regras);
+  } catch (error) {
+    return res.status(400).json({
+      erro: true,
+      mensagem: error.message,
+    });
+  }
+});
 
 module.exports = router;
